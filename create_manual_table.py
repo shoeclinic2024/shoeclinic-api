d887317eb7f90ffaf807fe4702b9a9153ef3d781
The Shoe Clinic <theshoeclinicsince2024@gmail.com>
2026-01-03 10:25:06 +0530

    Cleanup: Remove temporary table creation scripts

--- create_manual_table.py
+++
@@ -1,9 +0,0 @@
-from app import app, db
-from models import ManualTask
-
-if __name__ == "__main__":
-    with app.app_context():
-        # This will create the manual_task table if it doesn't exist
-        # It won't overwrite existing tables
-        db.create_all()
-        print("Database schema updated: ManualTask table ensured.")
